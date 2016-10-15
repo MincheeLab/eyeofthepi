1c11fdf59b943cbc6197381faaef7f6c370087ed
Gerald Estadieu <gestadieu@gmail.com>
2016-10-15 22:14:06 +0800

    conv.js a test file for avconv (to be removed) tentative roadmap time-lapse (test file for time-lapse) include avconv once time-lapse is completed

--- README.md
+++ README.md
@@ -4,3 +4,33 @@ An application to control the Raspberry Pi Camera
 ```
 npm install
 ```
+
+# Roadmap
+
+v0.1.0: Timelapse App
+1. Timelapse + video conversion
+2. Webapp to control (start/stop) timelapse: (M)EAN.js
+3. List/download all photos/videos
+
+v0.2.0: Timelapse settings
+1. allow configuration of camera (fx, brightness,...)
+2. Avconv settings: framerate, photos selection...
+
+v0.3.0: Preview
+1. Preview photo/video (webRTC, websocket?)
+
+v0.5.0: Photo/video
+1. Take photo
+2. Take video
+
+v0.6.0: Tests
+
+v0.7.0: hotspot
+
+
+Future plan (no particular order):
+1. auto upload to cloud services
+2. plugins
+3. animated gif (ImageMagick?): convert -delay 10 -loop 0 image*.jpg animation.gif
+4. Add an OLED screen for useful information display (IP, status,...)
+5. Timelapse with raspistill or cronjob to take single shots? can run longer?
--- conv.js
+++ conv.js
@@ -2,8 +2,8 @@ var fs = require("fs");
 var path = require("path");
 var avconv = require("avconv");
 
-var dir = "/Users/gestadieu/Documents/Devs/MincheeFactory/raspicamapp/shots/20161014-112346";
-var dir = __dirname + "/shots/";
+var timelapse = "20161015-041511";
+var dir = __dirname + "/shots/" + timelapse + "/";
   // avconv -r 10 -qscale 2 -i /home/pi/shots/photo-%03d.jpg video.mp4
   // avconv -framerate 25 -f image2 -i image-%03d.jpeg -b 65536k out.mov
   // avconv -framerate 25 -f image2 -i %04d.png -c:v h264 -crf 1 out.mov
@@ -11,31 +11,31 @@ var dir = __dirname + "/shots/";
   var params = [
     '-framerate', '25',
     '-f', 'image2',
-    '-i', dir + "photo-%04d.jpg",
+    '-i', dir + "pic-%04d.jpg",
     // '-c:v','libvpx', // libvpx|h264
     // '-deadline', 'realtime',
-    '-y', path.join(dir, "20161014-112346.mov")
+    '-y', path.join(dir, timelapse + ".mkv")
   ];
 
   // Returns a duplex stream
   var stream = avconv(params);
 
   // Anytime avconv outputs any information, forward these results to process.stdout
-  stream.on('message', function(data) {
-    process.stdout.write(data);
-  });
+  // stream.on('message', function(data) {
+  //   process.stdout.write(data);
+  // });
 
   stream.on('progress', function(progress) {
-    console.log('here is my progress...' + progress);
+    console.log(progress);
       /*
       Progress is a floating number between 0 ... 1 that keeps you
       informed about the current avconv conversion process.
       */
   });
 
-  stream.on('error', function(data) {
-    process.stderr.write(data);
-});
+//   stream.on('error', function(data) {
+//     process.stderr.write(data);
+// });
 
 stream.once('exit', function(exitCode, signal, metadata) {
   console.log('here I am officially DONE!!!');
