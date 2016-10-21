af23640c7bdff5704286276924d58d99cc484039
Gerald Estadieu <gestadieu@gmail.com>
2016-10-21 17:14:28 +0800

    small refactoring

--- index.js
+++ index.js
@@ -1,108 +1,25 @@
 var fs = require('fs');
 var path = require('path');
 
-var moment = require('moment');
-var jsonfile = require('json-file-plus');
+var io = require('socket.io')(http);
+var spawn = require('child_process').spawn;
+var proc;
+
 var express = require('express');
+var appRoutes = require('./routes/app');
 var app = express();
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
-var io = require('socket.io')(http);
-var spawn = require('child_process').spawn;
-var proc;
-var find = require('find');
 
-var Timelapse = require('./timelapse');
-
-// webapp
-app.use('/', express.static(path.join(__dirname, 'stream')));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+// app.use('/', express.static(path.join(__dirname, 'stream')));
+app.use('/', appRoutes);
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-
-// initial page, including preview
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-/*
-  list of timelapses from timelapses/ folder
-  each folder include: photos, animated gif thumbnail, config.json and video
-*/
-app.get('/timelapses', function(req, res) {
-  var configFiles = [];
-  // read timelapses folder and load all config.json
-  find.file(/\.json$/, path.join(__dirname, 'timelapses'), function(files){
-    if (!files.length) {
-      return res.send(configFiles);
-    }
-    files.forEach(function(file, idx) {
-      configFiles.push(JSON.parse(fs.readFileSync(file, {encoding: 'utf8'})));
-      if (idx >= files.length - 1) {
-        return res.send(configFiles);
-      }
-    }, this);
-  }).error(function(err) {
-    return res.status(500).send({message: 'Cannot read the timelapses folder', error: 500});
-  });
-});
-
-/*
-  create a new timelapse
-  TODO: 
-    - cronjob scheduling
-    - websockets to inform every photo and the video (if required)
-*/
-app.post('/timelapses', function(req, res) {
-  var tl = new Timelapse(req.body, function(err, t) {
-    tl.start(function(err, v){
-      if (err) {
-        return res.status(500).send({ message: 'Problem with the timelapse', error: '500'});
-      }
-      // transform photos into a video
-      if (req.body.transform) {
-        tl.transform(function(err){
-
-        });
-      }
-      console.log('========= timelapse completed ============');
-    })
-    return res.send(t);
-  });
-});
-
-/*
-  get a timelapse detail info
-*/
-app.get('/timelapses/:id', function(req, res) {
-  var id = req.params.id;
-  var tl = new Timelapse(id, function(err, t) {
-    return res.send(t);
-  });
-});
-
-/*
-  delete a timelapse
-*/
-app.delete('/timelapses/:id', function(req, res) {
-  var t = new Timelapse();
-  t.load(req.params.id, function(err){
-    t.remove(function(err){
-      if (!err) {
-        return res.status(200).send({message: 'Timelapse deleted.'});
-      }
-    })
-  })
-});
-
-/*
-  modify an existing timelapse (before it starts)
-*/
-app.put('/timelapses/:id', function(req, res) {
-
-});
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
---
+++ routes/app.js
@@ -0,0 +1,92 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var router = express.Router();
+var find = require('find');
+var Timelapse = require('../models/timelapse');
+
+router.get('/', function (req, res, next) {
+    res.render('index');
+});
+
+/*
+  GET /timelapses
+  list of timelapses from timelapses/ folder
+  each folder include: photos, animated gif thumbnail, config.json and video
+*/
+router.get('/timelapses', function (req, res, next) {
+  var configFiles = [];
+  // read timelapses folder and load all config.json
+  find.file(/\.json$/, path.join(__dirname, '../timelapses'), function(files){
+      if (!files.length) {
+      return res.send(configFiles);
+  }
+  files.forEach(function(file, idx) {
+    configFiles.push(JSON.parse(fs.readFileSync(file, {encoding: 'utf8'})));
+    if (idx >= files.length - 1) {
+      return res.send(configFiles);
+    }
+  }, this);
+}).error(function(err) {
+  return res.status(500).send({message: 'Cannot read the timelapses folder', error: 500});
+});
+});
+
+/*
+  GET /timelapses/:id
+  get a timelapse detail info
+*/
+router.get('/timelapses/:id', function(req, res, next) {
+  var id = req.params.id;
+  var tl = new Timelapse(id, function(err, t) {
+    return res.send(t);
+  });
+});
+
+/*
+  POST /timelapses
+  create a new timelapse
+  TODO: 
+    - cronjob scheduling
+    - websockets to inform every photo and the video (if required)
+*/
+router.post('/timelapses', function(req, res, next) {
+  var tl = new Timelapse(req.body, function(err, t) {
+    tl.start(function(err, v){
+      if (err) {
+        return res.status(500).send({ message: 'Problem with the timelapse', error: '500'});
+      }
+      // transform photos into a video
+      if (req.body.transform) {
+        tl.transform(function(err){
+
+        });
+      }
+      console.log('========= timelapse completed ============');
+    })
+    return res.send(t);
+  });
+});
+
+/*
+  delete a timelapse
+*/
+router.delete('/timelapses/:id', function(req, res, next) {
+  var t = new Timelapse();
+  t.load(req.params.id, function(err){
+    t.remove(function(err){
+      if (!err) {
+        return res.status(200).send({message: 'Timelapse deleted.'});
+      }
+    })
+  })
+});
+
+/*
+  modify an existing timelapse (before it starts)
+*/
+router.patch('/timelapses/:id', function(req, res, next) {
+
+});
+
+module.exports = router;
